75a18cc324e0a72195b28396e34eab857de87fa9
Kenzo de Oliveira Senna <kenzoandblade@gmail.com>
2025-12-04 10:17:01 -0400

    feat: enhance Cliente class with additional properties and validation methods (#7)

--- gaviaum_the_experience.js
+++ gaviaum_the_experience.js
@@ -10,13 +10,85 @@ class Agencia{
 class Cliente{
     // id
     nome
+    email
+    senha
+    cpf
+    cep
     saldo = 0
     movimentacoes = []
 
-    constructor(nome, saldo){
+    constructor (nome, saldo, cpf, email, senha, cep){
         this.nome = nome
         this.saldo = saldo
-        // this.id = sla
+        this.cpf = this.validaCPF(cpf)
+        this.cep = this.validarcep(cep)
+        this.email = email
+        this.senha = senha
+    }
+
+    validaCPF(cpf) {
+    var Soma = 0
+    var Resto
+
+    var strCPF = String(cpf).replace(/[^\d]/g, '')
+    
+    if (strCPF.length !== 11)
+        return false
+    
+    if ([
+        '00000000000',
+        '11111111111',
+        '22222222222',
+        '33333333333',
+        '44444444444',
+        '55555555555',
+        '66666666666',
+        '77777777777',
+        '88888888888',
+        '99999999999',
+        ].indexOf(strCPF) !== -1)
+        return false
+
+    for (i=1; i<=9; i++)
+        Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (11 - i);
+
+    Resto = (Soma * 10) % 11
+
+    if ((Resto == 10) || (Resto == 11)) 
+        Resto = 0
+
+    if (Resto != parseInt(strCPF.substring(9, 10)) )
+        return false
+
+    Soma = 0
+
+    for (i = 1; i <= 10; i++)
+        Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (12 - i)
+
+    Resto = (Soma * 10) % 11
+
+    if ((Resto == 10) || (Resto == 11)) 
+        Resto = 0
+
+    if (Resto != parseInt(strCPF.substring(10, 11) ) )
+        return false
+
+    return cpf
+    }
+
+    async validarcep(cepe) {
+        try {
+            const response = await fetch(`viacep.com.br/ws/${cepe}/json/`);
+            const cep = await response.json();
+            if (cep.logradouro){
+                return cepe
+            }
+            else{
+                return false
+            }
+        } catch (error) {
+            throw new Error(`Erro ao validar CEP: ${error}`);
+        }
     }
 
     saque(valor){
@@ -41,6 +113,8 @@ class Cliente{
         }
         
     }
+
+    
 
 }
 
